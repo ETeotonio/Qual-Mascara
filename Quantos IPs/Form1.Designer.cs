b8ded12a608730bb2af302c616265dd20b91c8b6
urashimaeliel <eliel.teotonio@gmail.com>
2016-03-22 21:42:43 -0300

    Arquivos do Projeto

---
+++ Quantos IPs/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Quantos_IPs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(55, 12);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(53, 20);
+            this.maskedTextBox1.TabIndex = 0;
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            this.maskedTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.maskedTextBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CIDR";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Quantos Ips?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            this.notifyIcon1.BalloonTipText = "Blablabla";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Quantos IPs";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 47);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Quantos IPs?";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Quantos IPs/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Quantos_IPs
+{
+    public partial class Form1 : Form
+    {
+        bool hidden=false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+        public string CIDR2DECIMAL(int cidr)
+        {
+            string[] decim = new string[4];
+
+            // We go through each octagon in the decimal address
+            for (int i = 0; i < 4; i++)
+            {
+                if (cidr > 8)
+                {
+                    decim[i] = "255";
+                    cidr -= 8;
+                }
+                else
+                {
+                    int temp = 0;
+                    for (int a = 7; cidr > 0; a--, cidr--)
+                    {
+                        temp += (int)Math.Pow(2, a);
+                    }
+                    decim[i] = temp.ToString();
+                }
+            }
+            return decim[0] + "." + decim[1] + "." + decim[2] + "." + decim[3];
+        }
+        public void qualIP()
+        {
+            if (maskedTextBox1.Text != "")
+            {
+                try
+                {
+                    int cidr = Convert.ToInt32(maskedTextBox1.Text);
+                    if (cidr < 32 && cidr > 0)
+                    {
+                        string netmask = CIDR2DECIMAL(cidr);
+                        int quantidade = 32 - cidr;
+                        MessageBox.Show(" IPs Disponiveis: " + (Math.Pow(2, Convert.ToDouble(quantidade))).ToString() + "\n Hosts Válidos: " + (Math.Pow(2, Convert.ToDouble(quantidade)) - 2).ToString() + "\n Mascara: " + netmask);
+                    }
+                    else if (cidr == 32)
+                    {
+                        string netmask1 = CIDR2DECIMAL(cidr);
+                        MessageBox.Show("IPs Disponiveis: 1\n Hosts Válidos: 1" + "\n Mascara: " + netmask1);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Digite um valor valido!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Digite um valor valido!");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            qualIP();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = true;
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void notifyIcon1_DoubleClick_1(object sender, EventArgs e)
+        {
+            if (hidden)
+            {
+                this.Show();
+                hidden = false;
+            }
+            else
+            {
+                this.Hide();
+                hidden = true;
+            }
+        }
+
+        private void maskedTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                qualIP();
+            }
+        }
+    }
+}
